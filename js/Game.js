3a5d4b1d571a2ad1c833213e5ffe62c290cd7d19
Joan Ques <jqb00@iesemilidarder.com>
2020-10-09 20:31:04 +0200

    0.1

---
+++ js/Game.js
@@ -0,0 +1,10 @@
+class Game{
+    constructor(gname,gdate,gpegi,gdeveloper,gcategory){
+        this.name = gname;
+        this.date = gdate;
+        this.pegi = gpegi;
+        this.developer = gdeveloper;
+        this.category = gcategory;
+    }
+};
+export default Game;
